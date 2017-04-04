dc81f6998a6a6cfeae5a14ede29af8e02944b58f
Mateusz Murawski <matimaxi@o2.pl>
2017-04-04 23:37:51 +0200

    poprawki zegara

--- Strona/skrypty/timer.js
+++ Strona/skrypty/timer.js
@@ -3,8 +3,13 @@ function odliczanie()
 		var dzisiaj = new Date();
 		
 		var dzien = dzisiaj.getDate();
+		if (dzien<10) dzien = "0"+dzien;
+		
 		var miesiac = dzisiaj.getMonth()+1;
+		if (miesiac<10) miesiac = "0"+miesiac;
+		
 		var rok = dzisiaj.getFullYear();
+		if (rok<10) rok = "0"+rok;
 		
 		var godzina = dzisiaj.getHours();
 		if (godzina<10) godzina = "0"+godzina;
